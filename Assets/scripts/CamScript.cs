816de1d715f3de700687464225e19d68574d7bcd
Sam <shl638@nyu.edu>
2018-10-30 02:04:07 -0400

    Final Finish

---
+++ Assets/scripts/CamScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour
+{
+
+	public GameObject godObject;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (float.IsNaN(godObject.GetComponent<GodScript>().averageFloorLocation.x) )
+		{
+			transform.position = new Vector3(0, 200f, 0);
+		}
+		else
+		{
+			transform.position = new Vector3(
+				godObject.GetComponent<GodScript>().averageFloorLocation.x,
+				transform.position.y,
+				godObject.GetComponent<GodScript>().averageFloorLocation.z
+			);
+		}
+	}
+}
---
+++ Assets/scripts/GodScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GodScript : MonoBehaviour {
+
+	public List<Transform> floorList = new List<Transform>();
+
+	public Vector3 averageFloorLocation = Vector3.zero;
+
+	public Vector3 sumFloorLocation;
+
+	public int maxFloorNum = 500;
+
+	public bool maxFloorsHit;
+
+	public Transform pathMakerPrefab;
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		sumFloorLocation = Vector3.zero;
+		foreach (Transform floorObject in floorList)
+		{
+			sumFloorLocation += floorObject.position;
+		}
+
+		averageFloorLocation = sumFloorLocation / floorList.Count;
+
+		if (floorList.Count > maxFloorNum)
+		{
+			maxFloorsHit = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			foreach (Transform floorThing in floorList)
+			{
+				Destroy(floorThing.gameObject);
+				
+			}
+			floorList.Clear();
+			maxFloorsHit = false;
+			Instantiate(pathMakerPrefab, Vector3.zero, Quaternion.identity);
+			averageFloorLocation = Vector3.zero;
+		}
+		
+		
+		
+	}
+}
